8d9b9dbe675cf14484c260be67c6db14a7509f7e
itiligent <github@itiligent.com>
2023-09-27 10:44:08 +1000

    updated for Debian 12 plus Mongo 4.4 for Unifi 7.5.x support

--- Basic_UniFi_Install.sh
+++ Basic_UniFi_Install.sh
@@ -1,14 +1,15 @@
 # Based on UI documentation https://help.ui.com/hc/en-us/articles/220066768-UniFi-How-to-Install-and-Update-via-APT-on-Debian-or-Ubuntu
 
 # Install base needs and java 11
-sudo apt-get update && sudo apt-get install ca-certificates apt-transport-https openjdk-11-jre-headless
+echo "deb http://deb.debian.org/debian/ bullseye main" | sudo tee /etc/apt/sources.list.d/bullseye.list # Needed for Debian 12, openjdk-11-jre-headless no longer in distro
+sudo apt-get update && sudo apt-get install ca-certificates apt-transport-https openjdk-11-jre-headless gnupg
 
 # Add Ubiquiti & Mongo to the sources
 echo 'deb https://www.ui.com/downloads/unifi/debian stable ubiquiti' | sudo tee /etc/apt/sources.list.d/100-ubnt-unifi.list
-echo "deb https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+echo "deb https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 # Add trust for Ubiquiti and Mongo sources
-wget -qO - https://www.mongodb.org/static/pgp/server-3.4.asc | sudo apt-key add -
+wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 sudo wget -O /etc/apt/trusted.gpg.d/unifi-repo.gpg https://dl.ui.com/unifi/unifi-repo.gpg
 
 # Hold the java version for compatability so it does not break with updates
